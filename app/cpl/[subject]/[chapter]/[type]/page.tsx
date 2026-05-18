4149d10b32b027538aca73c8675796d778856bf4
Ghost Aviator <pankaj.pahil10@gmail.com>
2026-05-18 17:43:30 +0530

    Add notes for Air Regulations Chapter 15 (Airworthiness of Aircraft)

--- app/cpl/[subject]/[chapter]/[type]/page.tsx
+++ app/cpl/[subject]/[chapter]/[type]/page.tsx
@@ -86,6 +86,7 @@ export default async function Page({
       "air-regulations/ar-10": true,
       "air-regulations/ar-11": true,
       "air-regulations/ar-14": true,
+      "air-regulations/ar-15": true,
     };
     if (HTML_NOTES_CHAPTERS[`${subject.id}/${chapter.id}`]) {
       return (
