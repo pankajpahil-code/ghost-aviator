35e3bb38d7854951e83847663beb44faa7b8a6ca
Ghost Aviator <pankaj.pahil10@gmail.com>
2026-05-29 10:14:58 +0530

    Add notes for Air Regulations Chapter 16 (Operational Procedures)

--- app/cpl/[subject]/[chapter]/[type]/page.tsx
+++ app/cpl/[subject]/[chapter]/[type]/page.tsx
@@ -87,6 +87,7 @@ export default async function Page({
       "air-regulations/ar-11": true,
       "air-regulations/ar-14": true,
       "air-regulations/ar-15": true,
+      "air-regulations/ar-16": true,
     };
     if (HTML_NOTES_CHAPTERS[`${subject.id}/${chapter.id}`]) {
       return (
